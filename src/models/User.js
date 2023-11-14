2396a7c43f6a3837caec11fe77d926c6f1d4b35e
tanwasp <tanishwasp10@gmail.com>
2023-11-13 21:54:47 -0500

    added User table

--- src/models/User.js
+++ src/models/User.js
@@ -1,29 +1,26 @@
-// import { DataTypes } from 'sequelize';
-// import { sequelize } from '../config/db.js';
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.js';
 
-// const User = sequelize.define('User', {
-//     userid: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     username: {
-//       type: DataTypes.STRING,
-//       unique: true,
-//       allowNull: false
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       unique: true,
-//       allowNull: false
-//     },
-//     dateJoined: {
-//       type: DataTypes.DATE,
-//       defaultValue: sequelize.NOW
-//     }
-//   });
+const User = sequelize.define('User', {
+    firebaseUid: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    dateJoined: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW
+    }
+  });
   
--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,11 @@
 import { sequelize } from '../config/db.js';
-// import User from './User.js';
+import User from './User.js';
 import Review from './Review.js';
 import Restaurant from './Restaurant.js';
 
 // Set up associations
-// User.hasMany(Review, { foreignKey: 'userid' });
-// Review.belongsTo(User, { foreignKey: 'userid' });
+User.hasMany(Review, { foreignKey: 'userid' });
+Review.belongsTo(User, { foreignKey: 'userid' });
 
 Restaurant.hasMany(Review, { foreignKey: 'restaurantid', as: 'reviews' });
 Review.belongsTo(Restaurant, { foreignKey: 'restaurantid' });
